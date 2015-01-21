982e26f1c13f6cb0c1bc7d5924213b3c82c62224
Rick Carlino <rick.carlino@gmail.com>
2015-01-21 11:23:49 -0600

    Added gulp-react, and a more standard watch()

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require('gulp'),
   bower = require('main-bower-files'),
   serve = require('gulp-serve'),
-  watch = require('gulp-watch');
+  react = require('gulp-react');
 
 var paths = {
   build: './build/js',
   src: ['./src/js/**/**/*.js'],
+  jsx: './src/js/**/**/*.jsx',
   bower: bower
 };
 
 gulp.task('serve', serve('build'));
+
+gulp.task('jsx', function() {
+  return gulp.src(paths.jsx)
+    .pipe(react())
+    .pipe(gulp.dest(paths.build));
+});
 
 gulp.task('libs', function() {
   // Move bower main files into `build/js`
@@ -18,12 +25,16 @@ gulp.task('libs', function() {
     .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('watch', function() {
+gulp.task('src', function() {
   // Move `src/js/**/*.js` into `build/js`.
   // That's all it does.
   gulp.src(paths.src)
-    .pipe(watch(paths.src))
     .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('default', ['libs', 'watch', 'serve']);
+gulp.task('watch', function() {
+  gulp.watch(paths.src, ['src']);
+  gulp.watch(paths.jsx, ['jsx']);
+});
+
+gulp.task('default', ['watch', 'serve']);
